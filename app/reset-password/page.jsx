599a210263a65bd6b0dbd400559f85fef4c2fb3d
Derick <dericklievian@gmail.com>
2026-05-26 22:47:19 +0800

    wrapping useSearParams to suspense to fix vercel build

--- app/reset-password/page.jsx
+++ app/reset-password/page.jsx
@@ -4,7 +4,13 @@ import ResetPasswordPage from "@/components/ResetPasswordPage";
 const page = () => {
     return (
         <div className="flex justify-center items-center h-[calc(100vh-5rem)]">
-            <ResetPasswordPage />
+            <Suspense
+                fallback={
+                    <div className="text-xl">Loading security token...</div>
+                }
+            >
+                <ResetPasswordPage />
+            </Suspense>
         </div>
     );
 };
